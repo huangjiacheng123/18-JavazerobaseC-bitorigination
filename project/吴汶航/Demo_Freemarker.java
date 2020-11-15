0c65af003e1e737110f4a338c153bd5f65d222ca
wuwenhang <3128991660@qq.com>
2020-11-15 10:39:44 +0800

    第一周

---
+++ project/吴汶航/Demo_Freemarker.java
@@ -0,0 +1,37 @@
+package com.webtest.demo;
+
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+//import com.webtest.core.JavaMailTestListener;
+import com.webtest.core.WebTestListener;
+
+@Listeners(WebTestListener.class)
+public class Demo_Freemarker{
+	@Test
+    public void test1() {
+        Assert.assertEquals(1, 1);
+    }
+	
+	@Test
+    public void test2() {
+        Assert.assertEquals(1, 2);
+    }
+	
+	@Test
+    public void test3() {
+        Assert.assertEquals(2, 2);
+    }
+	
+	@Test
+    public void test4() {
+        Assert.assertEquals(2, 1);
+    }
+	
+	@Test
+    public void test5() {
+        Assert.assertEquals(3, 1);
+    }
+
+}
