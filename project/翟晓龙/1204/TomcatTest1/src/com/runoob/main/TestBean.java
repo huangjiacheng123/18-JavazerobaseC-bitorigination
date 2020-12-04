a782aa7defdb71afe1f3fe7dff5ad27cf00342fe
ZhaiXiaolong <44954545+ZhaiXiaolong@users.noreply.github.com>
2020-12-04 17:12:19 +0800

    Add files via upload

---
+++ project/翟晓龙/1204/TomcatTest1/src/com/runoob/main/TestBean.java
@@ -0,0 +1,12 @@
+package com.runoob.main;
+
+public class TestBean {
+   private String message = "²ËÄñ½Ì³Ì";
+ 
+   public String getMessage() {
+      return(message);
+   }
+   public void setMessage(String message) {
+      this.message = message;
+   }
+}
